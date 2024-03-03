da04baced67f8cb406c23fd37e049ca00824ac57
David <daviodiosub09@gmail.com>
2024-03-03 12:48:43 +0100

    README.md syntax testing

--- frontEnd/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"], // Purge unnecessary styles in production,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
